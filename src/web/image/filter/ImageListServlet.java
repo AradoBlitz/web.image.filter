6ec3dacf1ccb99d0f6245986f4d83543466c042d
AradoBlitz <schlachtnavaho@gmail.com>
2015-05-18 13:36:04 +0300

    Extracted ImageFilterApplication to field and init it in Servlet.init() method by stub.

--- src/web/image/filter/ImageListServlet.java
+++ src/web/image/filter/ImageListServlet.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,21 +22,37 @@ import org.apache.tomcat.jni.Buffer;
 public class ImageListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	ImageFilterApplication imgFApp = new ImageFilterApplication();
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ImageListServlet() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
+
+    
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		 try {
+				imgFApp.addImage(config.getServletContext().getResourceAsStream("/sample.jpg"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println(request.getRequestURL());
-		ImageFilterApplication imgFApp = new ImageFilterApplication();
-		imgFApp.addImage(getServletContext().getResourceAsStream("/sample.jpg"));
+
+		
 		String requestPath = request.getRequestURL().toString();
 		
 		if(requestPath.contains("/imagelist")){
